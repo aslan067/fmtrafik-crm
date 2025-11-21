3ab49297bc71bb6a94b0b967ece11d6ff6472c9d
faslan <40107696+aslan067@users.noreply.github.com>
2025-11-21 15:04:58 +0300

    Update page.js

--- src/app/catalog/[slug]/page.js
+++ src/app/catalog/[slug]/page.js
@@ -1,6 +1,4 @@
 'use client'
-// Dynamic route olduğunu belirt
-export const dynamic = 'force-dynamic'
 
 import { useState, useEffect } from 'react'
 import { useParams, useRouter } from 'next/navigation'
